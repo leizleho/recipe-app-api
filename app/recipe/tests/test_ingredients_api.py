0d26c8a12034fca22a6e1a930b6a62aed77c5ceb
Leizle <ho.leizle@gmail.com>
2020-05-20 21:00:29 -0700

    added feature to list ingredients

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -64,4 +64,4 @@ class PrivateIngredientsApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
-        self.assertEqual(res.data[0], ingredient.name)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -6,7 +6,7 @@ from recipe import views
 # /api/recipe/tags/1/
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
-
+router.register('ingredients', views.IngredientViewSet)
 app_name = 'recipe'
 
 urlpatterns = [
